f31222c84c156476a2c1a79d00f3d740a1c8fc84
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-02-06 15:54:51 +0100

    feat(app): Add basic stylings

--- src/test/MyTestForm.tsx
+++ src/test/MyTestForm.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { useForm } from '../hooks/useForm';
-import {FormikValues} from 'formik';
+import { FormikValues } from 'formik';
 import { object, string } from "yup";
+import '../form/Form.css'
 
 function MyForm() {
   const initialValues = {
@@ -21,37 +21,41 @@ function MyForm() {
   const { formik } = useForm(initialValues, validationSchema, onSubmit);
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formik.values.name}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.name && formik.errors.name && (
-          <div className="error">{formik.errors.name}</div>
-        )}
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.email && formik.errors.email && (
-          <div className="error">{formik.errors.email}</div>
-        )}
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <main className="main">
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">
+          Name:
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={formik.errors.name && 'input-err'}
+          />
+          {formik.touched.name && formik.errors.name && (
+            <div className="error">{formik.errors.name}</div>
+          )}
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={formik.errors.name && 'input-err'}
+          />
+          {formik.touched.email && formik.errors.email && (
+            <div className="error">{formik.errors.email}</div>
+          )}
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </main>
   );
 }
 
